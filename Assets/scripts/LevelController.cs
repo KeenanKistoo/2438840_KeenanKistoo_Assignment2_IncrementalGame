54b05cd6ef417087d705e9b337737ce7196418a0
KeenanKistoo <kistook19@gmail.com>
2023-04-23 22:28:49 +0200

    Upgrade Setup
    I have set up the basics for the upgrades.
    There are only basic upgrades for now though.
    Reason for minimal work: Unplanned power outage... sigh

---
+++ Assets/scripts/LevelController.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LevelController : MonoBehaviour
+{
+    [Header("Basic Upgrade:")]
+    public string[] upgradeName;
+    public int[] upgradeLevel;
+}
---
+++ Assets/scripts/Upgrade.cs
@@ -0,0 +1,21 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Upgrade : MonoBehaviour
+{
+    [Header("Coin Controller")]
+    public ClickBehaviour coinControl;
+
+    public void UpgradeTown(float cost)
+    {
+        
+    }
+
+    public void LevelTracker(int upgradeNum)
+    {
+
+    }
+
+}

ee66fa9d857b6efec54cde762cdf813d0d400377
KeenanKistoo <kistook19@gmail.com>
2023-04-25 17:32:13 +0200

    Completed the town name and click display
    The core mechanic is complete. I just need to make sure the multiplier is communicated to the player

--- Assets/scripts/ClickBehaviour.cs
+++ Assets/scripts/ClickBehaviour.cs
@@ -1,11 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-
+using UnityEngine.UI;
 public class ClickBehaviour : MonoBehaviour
 {
+    [Header("Core Mechanics:")]
     public float clickCount;
     public float clickMultiplier;
+
+    [Header("Display:")]
+    public Text clickText;
 
     private void Start()
     {
@@ -15,5 +19,21 @@ public class ClickBehaviour : MonoBehaviour
     public void Click()
     {
         clickCount += clickMultiplier;
+        DecimalControl();
+    }
+
+    public void DecimalControl()
+    {
+       
+        if(clickCount > 1000) 
+        {
+            float totalClicks = clickCount;
+            float convClicks = totalClicks;
+            clickText.text = (clickCount / 1000).ToString();
+        }
+        else
+        {
+            clickText.text = clickCount.ToString();
+        }
     }
 }

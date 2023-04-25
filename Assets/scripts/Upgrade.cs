a02022c9bb47b3fa3f2836d8ab0ed8fdd189727f
KeenanKistoo <kistook19@gmail.com>
2023-04-26 01:09:31 +0200

    Button display complete
    Programmed the buttons to grab and display information

--- Assets/scripts/Upgrade.cs
+++ Assets/scripts/Upgrade.cs
@@ -5,12 +5,22 @@ using UnityEngine.UI;
 
 public class Upgrade : MonoBehaviour
 {
-    [Header("Coin Controller:")]
+    [Header("Scripts:")]
     public ClickBehaviour coinControl;
+    public LevelController lvlControl;
 
     [Header("Trackers:")]
     public float costTrack;
-    
+    public int lvlArray;
+
+    [Header("UI Elements:")]
+    public Text costText;
+    public Text lvlText;
+
+    private void Start()
+    {
+        SetUp();
+    }
 
     public void UpgradeTown()
     {
@@ -32,4 +42,9 @@ public class Upgrade : MonoBehaviour
 
     }
 
+    public void SetUp()
+    {
+        costText.text = costTrack.ToString();
+        lvlText.text = "X" + lvlControl.upgradeLevel[lvlArray].ToString();
+    }
 }
